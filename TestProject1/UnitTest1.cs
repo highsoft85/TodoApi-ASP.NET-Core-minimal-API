125b24ada9f255aac5b869f54fc64e81522442f4
HighSoft <highsoft85@outlook.com>
2024-06-11 16:36:03 +0200

    Initial Commit
    Initial Model, Db, API routes are implemented

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+
+        //public async Task GetAllTodos_ReturnsOkOfTodosResult()
+        //{
+        //    // Arrange
+        //    var db = CreateDbContext();
+
+        //    // Act
+        //    var result = await TodosApi.GetAllTodos(db);
+
+        //    // Assert: Check for the correct returned type
+        //    Assert.IsType<Ok<Todo[]>>(result);
+        //}
+    }
+}
